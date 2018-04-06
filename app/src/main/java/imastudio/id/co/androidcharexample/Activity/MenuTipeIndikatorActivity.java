ed7a12b9e7551839e27d890a58614452b662c3fc
Programmer Muslim <rizki@imastudio.co.id>
2018-04-06 09:04:35 +0700

    add

---
+++ app/src/main/java/imastudio/id/co/androidcharexample/Activity/MenuTipeIndikatorActivity.java
@@ -0,0 +1,102 @@
+package imastudio.id.co.androidcharexample.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import superheroku.id.co.kolakaapps.R;
+import superheroku.id.co.kolakaapps.adapter.AdapterTipeIndikator;
+import superheroku.id.co.kolakaapps.helper.HeroHelper;
+import superheroku.id.co.kolakaapps.helper.No_Internet;
+import superheroku.id.co.kolakaapps.helper.SessionManager;
+import superheroku.id.co.kolakaapps.modelapi.RssJ273GetAllIndikator.DataItemJ273TipeIndikator;
+import superheroku.id.co.kolakaapps.modelapi.RssJ273GetAllIndikator.RssJ273GetAllIndikator;
+import superheroku.id.co.kolakaapps.network.MyRetrofitClient;
+import superheroku.id.co.kolakaapps.network.RestApi;
+
+public class MenuTipeIndikatorActivity extends AppCompatActivity {
+    public static List<DataItemJ273TipeIndikator> dataItemJ23RiwayatLelangs;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_wisata);
+        ButterKnife.bind(this);
+        dataItemJ23RiwayatLelangs = new ArrayList<>();
+
+        if (!HeroHelper.isOnline(getApplicationContext())) {
+            startActivity(new Intent(getApplicationContext(), No_Internet.class));
+           finish();
+
+        } else {
+
+            getListData();
+
+        }
+    }
+
+    private void getListData() {
+        try {
+            RestApi api = MyRetrofitClient.getInstanceRetrofit2();
+            SessionManager sesi = new SessionManager(getApplicationContext());
+            String nUser = sesi.getIdUser();
+            Log.i("RSMUser : ", nUser);
+
+            Call<RssJ273GetAllIndikator> call = api.getAllTipeIndikator();
+            call.enqueue(new Callback<RssJ273GetAllIndikator>() {
+                @Override
+                public void onResponse(Call<RssJ273GetAllIndikator> call, Response<RssJ273GetAllIndikator> response) {
+//                    Log.d("onResponse", response.body().toString());
+
+                    String r = response.body().getResult();
+//                String nData = response.body().getDataProfilUSer().getLevel();
+                    Log.d("adaa", response.body().toString());
+
+                    if (r.equalsIgnoreCase("true")) {
+
+
+                        dataItemJ23RiwayatLelangs = response.body().getData();
+//                    layoutManager = new LinearLayoutManager(getApplicationContext());
+                        AdapterTipeIndikator recyclerViewAdapter =
+                                new AdapterTipeIndikator(dataItemJ23RiwayatLelangs, MenuTipeIndikatorActivity.this);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(MenuTipeIndikatorActivity.this));
+
+//                    recyclerView.setLayoutManager(layoutManager);
+////                        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+                        recyclerView.setAdapter(recyclerViewAdapter);
+
+                    }else {
+
+                    }
+
+
+
+                }
+
+                @Override
+                public void onFailure(Call<RssJ273GetAllIndikator> call, Throwable t) {
+
+                }
+            });
+
+//
+        } catch (Exception e) {
+        }
+    }
+
+}
--- app/src/main/java/imastudio/id/co/androidcharexample/helper/MyConstant.java
+++ app/src/main/java/imastudio/id/co/androidcharexample/helper/MyConstant.java
@@ -6,7 +6,7 @@ package imastudio.id.co.androidcharexample.helper;
 
 public class MyConstant {
     public static final String BASE_URL = "";
-    public static final String BASE_URL2 = "http://192.168.100.13/api_serverkolaka/index.php/api/";
+    public static final String BASE_URL2 = "http://192.168.100.4/api_serverkolaka/index.php/api/";
      public static final int REQ_FILE_CHOOSE = 1;
     public static final int STORAGE_PERMISSION_CODE = 2;
     public static final String UPLOAD_URL ="http://192.168.20.70/db_makananlanjutan/uploadmakanan.php" ;
