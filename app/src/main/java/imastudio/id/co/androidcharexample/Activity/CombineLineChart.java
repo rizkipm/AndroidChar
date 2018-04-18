190a7fa0286f7cf153607e3918f7eeafc14a6e1c
Programmer Muslim <rizki@imastudio.co.id>
2018-04-18 16:34:45 +0700

    add

---
+++ app/src/main/java/imastudio/id/co/androidcharexample/Activity/CombineLineChart.java
@@ -0,0 +1,15 @@
+package imastudio.id.co.androidcharexample.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import imastudio.id.co.androidcharexample.R;
+
+public class CombineLineChart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combine_line_chart);
+    }
+}
