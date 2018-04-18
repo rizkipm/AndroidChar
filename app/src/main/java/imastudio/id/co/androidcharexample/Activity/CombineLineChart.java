accce5f92b1cf7774347be55d36667285e8b4547
Programmer Muslim <rizki@imastudio.co.id>
2018-04-18 17:56:29 +0700

    add

--- app/src/main/java/imastudio/id/co/androidcharexample/Activity/CombineLineChart.java
+++ app/src/main/java/imastudio/id/co/androidcharexample/Activity/CombineLineChart.java
@@ -1,15 +1,150 @@
 package imastudio.id.co.androidcharexample.Activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 import imastudio.id.co.androidcharexample.R;
 
 public class CombineLineChart extends AppCompatActivity {
+    private CombinedChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combine_line_chart);
+
+        mChart = (CombinedChart) findViewById(R.id.chart1);
+        mChart.getDescription().setText("This is testing Description");
+        mChart.setBackgroundColor(Color.WHITE);
+        mChart.setDrawGridBackground(true);
+        mChart.setDrawBarShadow(true);
+        mChart.setHighlightFullBarEnabled(true);
+
+        // draw bars behind lines
+        mChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.BAR,  CombinedChart.DrawOrder.LINE
+        });
+
+        Legend l = mChart.getLegend();
+        l.setWordWrapEnabled(true);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mMonths[(int) value % mMonths.length];
+            }
+        });
+
+        CombinedData data = new CombinedData();
+
+        data.setData( generateLineData());
+        data.setData(generateBarData());
+
+        xAxis.setAxisMaximum(data.getXMax() + 0.25f);
+        mChart.setData(data);
+        mChart.invalidate();
+    }
+    protected String[] mMonths = new String[] {
+            "Jan", "Feb", "Mar", "Apr", "May", "June"
+    };
+
+    private LineData generateLineData() {
+
+        LineData d = new LineData();
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        entries = getLineEntriesData(entries);
+
+        LineDataSet set = new LineDataSet(entries, "Line");
+        //set.setColor(Color.rgb(240, 238, 70));
+        set.setColors(ColorTemplate.COLORFUL_COLORS);
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(240, 238, 70));
+        set.setCircleRadius(5f);
+        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(240, 238, 70));
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        d.addDataSet(set);
+
+        return d;
+    }
+
+    private ArrayList<Entry> getLineEntriesData(ArrayList<Entry> entries){
+        entries.add(new Entry(1, 20));
+        entries.add(new Entry(2, 10));
+        entries.add(new Entry(3, 8));
+        entries.add(new Entry(4, 40));
+        entries.add(new Entry(5, 37));
+
+        return entries;
+    }
+
+    private BarData generateBarData() {
+
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+        entries = getBarEnteries(entries);
+
+        BarDataSet set1 = new BarDataSet(entries, "Bar");
+        //set1.setColor(Color.rgb(60, 220, 78));
+        set1.setColors(ColorTemplate.COLORFUL_COLORS);
+        set1.setValueTextColor(Color.rgb(60, 220, 78));
+        set1.setValueTextSize(10f);
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        float barWidth = 0.45f; // x2 dataset
+
+
+        BarData d = new BarData(set1);
+        d.setBarWidth(barWidth);
+
+
+        return d;
+    }
+
+    private ArrayList<BarEntry> getBarEnteries(ArrayList<BarEntry> entries){
+        entries.add(new BarEntry(1, 25));
+        entries.add(new BarEntry(2, 30));
+        entries.add(new BarEntry(3, 38));
+        entries.add(new BarEntry(4, 10));
+        entries.add(new BarEntry(5, 15));
+        return  entries;
     }
 }
