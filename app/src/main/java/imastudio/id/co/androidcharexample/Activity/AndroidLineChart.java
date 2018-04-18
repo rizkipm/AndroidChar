04e6ba66f1f2d8e2f36e08678a873cf28331a080
Programmer Muslim <rizki@imastudio.co.id>
2018-04-18 14:12:49 +0700

    add

---
+++ app/src/main/java/imastudio/id/co/androidcharexample/Activity/AndroidLineChart.java
@@ -0,0 +1,13 @@
+package imastudio.id.co.androidcharexample.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidLineChart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_line_chart);
+    }
+}
